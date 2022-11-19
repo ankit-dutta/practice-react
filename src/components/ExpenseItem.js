932b7259b4aaee6410df622e94701b985e52054a
ankit-dutta <ankitdutta965@gmail.com>
2022-11-19 22:14:00 +0530

    react-practice

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,16 @@
+function ExpenseItem(){
+    return (
+        <>
+        <h1>Expense Items</h1>
+        
+           <div>March 28th 2021</div>
+           
+           <div>
+            <h2>Car Insurance</h2>
+            <div>$294.67</div>
+           </div>
+        </>
+    )
+}
+
+export default ExpenseItem;
